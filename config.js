3d585efdc44e4132b01f7fd894c50bcd85f549fe
Brance Boren <branceboren@gmail.com>
2014-11-22 02:03:13 -0700

    Version 0.1.0 Works when ran manually or with a setTimeout.  Planning on setting up the webhook in later release.  This may turn into an npm

---
+++ config.js
@@ -0,0 +1,102 @@
+var readline = require('readline'),
+	fs = require('fs'),
+	mkdirp = require('mkdirp'),
+	config_obj = {},
+	config_path = __dirname+'/config/test/',
+	config_file = config_path+'config.json';
+if(!fs.existsSync(config_path)){
+	mkdirp.sync(config_path);
+}
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+var q = [
+	{
+		title:'SendHub API key',
+		desc: '(found on the account settings page at www.sendhub.com)',
+		json_key:'sh_api',
+		'default': 'https://api.sendhub.com',
+		action: null
+	},
+	{
+		title:'SendHub account phone number',
+		desc: '(the number you used to sign up)',
+		json_key:'phone_number',
+		'default': null,
+		action: null
+	},
+	{
+		title:'API Version',
+		desc: '(most likely v1)',
+		json_key:'api_ver',
+		'default': 'v1',
+		action: null
+	},
+	{
+		title:'API Key',
+		desc: '(Found in the settings page as well)',
+		json_key:'api_key',
+		'default': null,
+		action: null
+	},
+	{
+		title:'unique passphrase',
+		desc: '(what you\'ll prepend to every text)',
+		json_key:'password',
+		'default': null,
+		action: null
+	},
+	{
+		title:'list of valid phone numbers',
+		desc: '(phone numbers you allow to communicate with your server MAKE SURE TO ADD A +1 for US country code and add spaces between each phone number)',
+		json_key:'valid_phones',
+		'default': [],
+		action: function(a){
+			if(a.length > 0){
+				return a.split(' ');
+			}
+		}
+	}
+]; 
+var i = 0;
+ask(i);
+function ask(i){
+	if(i == q.length){
+		console.log('CONFIGURATION COMPLETE');
+		rl.close();
+		writeConfigFile(config_obj);
+	} else{
+		rl.question(q[i].title+' '+q[i].desc+': ', function(answer) {
+			if(answer.length == 0 && q[i].default != null){
+				answer = q[i].default;
+			}
+			if(q[i].action !== null){
+				answer = q[i].action(answer);
+			}
+			config_obj[q[i].json_key] = answer;
+			i++;
+			ask(i);
+		});
+	}
+}
+
+function writeConfigFile(obj){
+	var json = JSON.stringify(obj);
+	fs.writeFile(config_file, json, function(err){
+		if(err){
+			console.log(err);
+		} else{
+			console.log("Successfully wrote config file");
+		}
+	});
+}
+// {
+// 	"sh_api": "https://api.sendhub.com",
+// 	"phone_number": "4352517720",
+// 	"api_ver": "v1",
+// 	"api_key": "dfb4df8893fd720132ae089d6bf66ec80964e01c",
+// 	"password": "Nyan cat 1337",
+// 	"valid_phones": ["+14352517720"]
+// }
---
+++ sendhub.js
@@ -0,0 +1,195 @@
+var request = require('request'),
+	fs = require('fs'),
+	sys = require('sys'),
+	exec = require('child_process').exec,
+	config = JSON.parse(fs.readFileSync("config/bconfig.json")), // or +process.argv[process.argv.length - 1]
+	sh_api = config.sh_api,
+	number = config.phone_number,
+	apikey = config.api_key,
+	api_ver = config.api_ver,
+	identify = '/?username='+number+'&api_key='+apikey
+	output = __dirname+'/output.json',
+	pass_regex = new RegExp('^'+config.password+'[ ]*'),
+	valid_phones = config.valid_phones;
+var fileToJson = function(f){
+	return JSON.parse(fs.readFileSync(f));
+}
+
+function sendhubGet(source){
+	var uri = sh_api+'/'+api_ver+'/'+source;
+	console.log(uri);
+
+	var options = {
+		uri: uri+identify, 
+		method: 'GET'
+	};
+
+	console.log(options);
+	req = request(options, function (error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	        console.log(body);
+			fs.writeFileSync(output, body); // or just use callback
+			main();
+			// fs.writeFile(output, body, function(err){
+// 				if(err){
+// 					console.log(err);
+// 				} else{
+// 					console.log("successfully wrote file");
+// 				}
+// 			});
+	    } else{
+	    	// console.log(error);
+	    	// console.log(response);
+	    	console.log('something went wrong...');
+	    	console.log('Status Code: '+response.statusCode);
+	    }
+	});
+}
+
+sendhubGet('inbox');
+// main();
+function main(){
+	var unread = sortByDate(getUnread());
+	for(var i=0; i<unread.length; i++){
+		var unread = unread[i],
+			txt = unread.text,
+			msg_id = unread.id,
+			sender_id = unread.contacts[0].id_str; // change to handle multiple
+			valid = isAuthorized(unread),
+			command = getCommand(txt);
+		if(valid){
+			exec(command, function(error, stdout, stderr){
+				var msg = stderr ? stderr : error ? 'error with command execution': stdout;
+				if(msg.length > 160){ // maybe take out later
+					msg = msg.substring(0,78)+'...'+msg.substring(msg.length-79, msg.length);
+				}
+				var post_data = {
+					'contacts': [sender_id],
+					'text': msg
+				};
+				sendhubPost(post_data, 'messages');
+				markAsRead(msg_id);
+			});
+		} else {
+			console.log("not a valid phone or password");
+		}
+	}	
+}
+
+function sendhubPost(post_data, source){
+	var uri = sh_api+'/'+api_ver+'/'+source;
+	console.log(uri);
+
+	var options = {
+		uri: uri+identify, 
+		method: 'POST', 
+		json: post_data
+	};
+
+	console.log(options);
+	req = request(options, function (error, response, body) {
+	    if (!error && response.statusCode == 201) {
+	        console.log(body);
+			console.log('request successful');
+			// fs.writeFileSync(output, body); // or just use callback
+			// fs.writeFile(output, body, function(err){
+// 				if(err){
+// 					console.log(err);
+// 				} else{
+// 					console.log("successfully wrote file");
+// 				}
+// 			});
+	    } else{
+	    	console.log('something went wrong...');
+	    	console.log('Status Code: '+response.statusCode);
+	    }
+	});
+}
+
+function markAsRead(mes_id){
+	var uri = sh_api+'/'+api_ver+'/messages/'+mes_id;
+	console.log(uri);
+
+	var options = {
+		uri: uri+identify, 
+		method: 'PUT', 
+		json: {unread: false}
+	};
+
+	console.log(options);
+	req = request(options, function (error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	        console.log(body);
+			console.log('request successful');
+	    } else{
+	    	console.log('something went wrong...');
+	    	console.log('Status Code: '+response.statusCode);
+	    }
+	});
+}
+
+function isAuthorized(msg){
+	var contacts = msg.contacts;
+	var phone_arr = [];
+	for(var i=0; i<contacts.length; i++){
+		if(valid_phones.indexOf(contacts.number) > -1){
+			phone_arr.push(contacts[i].number);
+		}
+	}
+	var phone_match = phone_arr.every(function(val){
+		return valid_phones.indexOf(val) >=0;
+	});
+	var pass_match = msg.text.match(pass_regex) !== null ? true : false;
+	return pass_match && phone_match;
+}
+
+function getCommand(msg){
+	return msg.replace(pass_regex, '');
+}
+
+function getUnread(){ // have json as param???
+	var json = fileToJson('output.json'),
+		messages = json.objects;
+	return messages.filter(function(el){
+		return el.unread == true;
+	});
+}
+
+function getMostRecent(amt){ // have json as param???
+	var json = fileToJson('output.json'),
+		messages = json.objects
+		amt = typeof amt !== 'undefined' ? amt : 5,
+		sorted_dates = [];
+	sorted_dates = sortByDate(messages, 'd')
+	return sorted_dates.slice(0, amt);
+}
+
+function sortByDate(arr, order){
+	var order = typeof order !== 'undefined' ? order : 'd';
+	if(order == 'a'){
+		arr.sort(function(a,b){
+			var d1 = new Date(a.sent),
+				d2 = new Date(b.sent);
+			if(d1<d2){
+				return -1;
+			} else if(d1 == d2){
+				return 0;
+			} else{
+				return 1;
+			}
+		});
+	} else if(order == 'd'){ // don't judge :P
+		arr.sort(function(a,b){
+			var d1 = new Date(a.sent),
+				d2 = new Date(b.sent);
+			if(d1<d2){
+				return 1;
+			} else if(d1 == d2){
+				return 0;
+			} else{
+				return -1;
+			}
+		});
+	}
+	return arr;
+}
